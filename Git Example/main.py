4822dc885834579817d87f7aef40b42e7f3d20b8
Mirza Tulaib <mirzatulaibyasir@gmail.com>
2024-01-15 17:46:44 +0500

    another puch

--- Git Example/main.py
+++ Git Example/main.py
@@ -1,3 +1,34 @@
-print('hello world!')
+#Problem 1
+def multiplication_or_sum(num1, num2):
+    product = num1 * num2
+    if product <= 1000:
+        return product
+    else:
+        return num1 + num2
 
-print('hello again')
+result = multiplication_or_sum(20, 30)
+print("The result is", result)
+result = multiplication_or_sum(40, 30)
+print("The result is", result)
+
+
+#Problem 2
+previous_num = 0
+
+for i in range(1, 11):
+    x_sum = previous_num + i
+    print("Current Number", i, "Previous Number ", previous_num, "Sum: ", x_sum)
+
+    previous_num = i
+
+
+#Problem 3
+word = input("Enter word")
+print("Original String:", word)
+
+size = len(word)
+
+print("Printing only even index chars")
+for i in range(0, size - 1, 2):
+    print("index[", i, "]", word[i])
+
